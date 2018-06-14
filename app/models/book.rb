86eb50f8302a7f93d7f01df3f4b94ddd3b6b78de
matsuhisa <matsuhisa+github@gmail.com>
2018-06-14 18:48:48 +0900

    Add Book#new_arrivals?

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  def new_arrivals?
+    (created_at..created_at.since(7.days)).cover?(Time.zone.now)
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#new_arrivals?' do
+    subject do
+      travel_to current_time do
+        book.new_arrivals?
+      end
+    end
+
+    let(:current_time) { Time.zone.now }
+
+    context '現在のとき' do
+      let(:book) { create(:book) }
+
+      it { is_expected.to be true }
+    end
+
+    context '登録が7日前のとき' do
+      let(:book) { create(:book, created_at: current_time - 7.days) }
+
+      it { is_expected.to be true }
+    end
+
+    context '登録が8日前のとき' do
+      let(:book) { create(:book, created_at: current_time - 8.days) }
+
+      it { is_expected.to be false }
+    end
+  end
 end
