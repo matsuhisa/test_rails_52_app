5069159802d8c653f0c7aef7a8c2b21fc4f107db
matsuhisa <matsuhisa+github@gmail.com>
2019-07-14 11:54:38 +0900

    Add validates

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+
   def new_arrivals?
     (created_at..created_at.since(7.days)).cover?(Time.zone.now)
   end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module TestRails52App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.action_view.field_error_proc = Proc.new { |html_tag, _instance| %Q(#{html_tag}).html_safe }
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, _instance| 
+      # Nokogiri::HTML.fragment(html_tag).search('input', 'textarea', 'select').add_class('is-error').to_html.html_safe
+      html_field = Nokogiri::HTML::DocumentFragment.parse(html_tag).children.attribute("aria-invalid", true)
+      html_field.to_s.html_safe
+    end
+
   end
 end
